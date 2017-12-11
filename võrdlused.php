5f8f4350d5da600c7a8458af74db6e3b14195d41
eveliiskallas <eveliis.kallas@gmail.com>
2017-12-11 12:41:28 +0200

    Defineeritud vajalikud muutujad

---
+++ võrdlused.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eveliis.kallas
+ * Date: 11.12.2017
+ * Time: 12:38
+ */
+// Defineeri kaheksa muutujat, mille väärtusteks on täisarvud, reaalarvud, sõned ja boolean väärtused
+$var1 = 5;
+$var2 = 5.0;
+$var3 = '5';
+$var4 = 2;
+$var5 = 2.0;
+$var6 = '2';
+$var7 = True;
+$var8 = False;
