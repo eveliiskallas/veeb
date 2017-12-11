7dff0ba58bd9170fc8619a584ab23ee1f1a93f45
eveliiskallas <eveliis.kallas@gmail.com>
2017-12-11 12:29:44 +0200

    Defineeritud ja väljastatud muutuja

---
+++ mina.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eveliis.kallas
+ * Date: 11.12.2017
+ * Time: 12:11
+ */
+ // üherealine kommentaar
+/*
+mitme
+realine
+kommentaar
+*/
+
+/*
+Defineeri oma nimega muutuja, mille väärtuseks on Sinu Eesnimi ja väljasta antud muutuja väärtus ekraani koos
+lisatekstiga
+Näiteks :
+Minu nimi on  Eesnmi
+*/
+
+$eveliis = 'Eveliis';
+// echo
+echo 'Minu nimi on ' .$eveliis. '<br / >'; // see on parim varjant
+// print funktsioon
+print 'Minu nimi on ' .$eveliis. '<br / >' ;
+print("Minu nimi on $eveliis <br / >");
