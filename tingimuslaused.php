9f8359fc2260fa3e75bcc99618673270dfaada32
eveliiskallas <eveliis.kallas@gmail.com>
2017-12-13 09:03:45 +0200

    Kirjeldatud seostatud tingimuslaused

--- tingimuslaused.php
+++ tingimuslaused.php
@@ -13,3 +13,17 @@ if ($ilm == 'Sajab'){
 }
 echo 'Väljas '.$ilm.'<br />';
 echo 'Minul on  '.$tuju.' tuju<br />';
+//
+$vanus = rand(0, 100);
+echo $vanus;
+if($vanus >= 0 and $vanus < 11) {
+    $staatus = 'laps';
+} elseif ($vanus > 10 and $vanus < 18) {
+    $staatus = 'nooruk';
+} elseif ($vanus > 17 and $vanus < 65) {
+    $staatus = 'täiskasvanu';
+} else {
+    $staatus = 'senioor';
+}
+echo 'Olen  '.$vanus.' aastat vana.<br />';
+echo 'Olen '.$staatus.'<br />';
