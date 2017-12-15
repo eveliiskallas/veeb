9469fda222212705f7cbb070fa312f50c217f8af
eveliis.kallas <eveliis.kallas@khk.ee>
2017-12-15 10:47:57 +0200

    Arvu väljastamine on täiendatud värvi valikuga

--- tsüklid.php
+++ tsüklid.php
@@ -5,6 +5,16 @@
  * Date: 15.12.2017
  * Time: 10:29
  */
+/**
+ * Täienda antud lahendust nii, et paaris arvud oleks punast värvi ja paaritud arvud sinist värvi
+ */
+$varv = '';
 for ($arv = 1; $arv <=10; $arv++) {
-    echo $arv.'<br />';
+    if($arv % 2 == 0) {
+        $varv = 'red';
+    } else {
+        $varv = 'blue';
+    }
+    echo '<p style="color:'.$varv.';">'.$arv . '</p>';
 }
+
