122996dbeb1270b50663f9e078c9c9c0f168bc24
eveliis.kallas <eveliis.kallas@khk.ee>
2018-01-09 12:37:42 +0200

    Raamatute massiivi väljastamine tabelina

--- massiivid2.php
+++ massiivid2.php
@@ -53,3 +53,83 @@ foreach ($opilased as $opilane) {
     }
     echo '----------'.'<br />';
 }
+
+/*
+ * Katsetamiseks raamatud kirjeldav massiiv kujul:
+ * array(
+ *      array(
+ *          'title' => 'raamat',
+ *          'author' => 'autor',
+ *          'print' => 'trükikoda',
+ *          'status' => 'seisund'
+ *      )
+ * );
+ * Loo funktsioon nimega otsiRaamat, mille parameetrina on raamatute
+ * massiiv ja seisund, funktsioon tagastab massiivi,
+ * kus on raamatud vastava seisundiga (seisund tähendab,
+ * kas raamat on välja antud või mitte).
+ * Loo funktsioon nimega raamatuteTabel, mis võtab parameetrina massiivi, ja
+ * väljastab tulemused tabeli kujul. Tabeli pealkirjaks
+ * on massiivi võtmed (pealkirja loomiseks võib kasutada eraldi funktsiooni)
+ * */
+
+function raamatuteTabel($raamatud){
+    echo '<table border="1">';
+    $pealkiriValjastatud = 0;
+    foreach($raamatud as $raamat){
+        echo '<tr>';
+        if(!$pealkiriValjastatud){
+            $pealkiri = array_keys($raamat);
+        foreach ($pealkiri as $nimetus){
+            echo '<th>';
+            echo $nimetus;
+            echo'</th>';
+        }
+        echo '</tr>';
+        $pealkiriValjastatud = true;
+        }
+        foreach ($raamat as $element => $vaartus) {
+             echo '<td>';
+             echo $vaartus;
+             echo'</td>';
+            }
+        echo '</tr>';
+    }
+    echo '</table>';
+}
+
+
+$raamatud = array(
+    array(
+        'title' => 'Peppa Goes To London',
+        'author' => 'Ladybird',
+        'print' => 'Penguin',
+        'status' => 'valjas'
+    ),
+    array(
+        'title' => 'Toto, Joonas ja püha Mimoos',
+        'author' => 'Kristiina K.',
+        'print' => 'Heli Kirjastus OÜ',
+        'status' => 'sees'
+    ),
+    array(
+        'title' => 'Lugusid loomadest',
+        'author' => 'Ernest Thompson Seton',
+        'print' => 'Ernest Thompson Seton',
+        'status' => 'sees'
+    ),
+    array(
+        'title' => 'Miljon miksi ja miljon vastust',
+        'author' => '',
+        'print' => 'TEA Kirjastus',
+        'status' => 'sees'
+    ),
+    array(
+        'title' => 'Teadmiste Puu: Maailma Loodus',
+        'author' => 'AJ Wood, Mike Jolley',
+        'print' => 'Pikoprint',
+        'status' => 'valjas'
+    )
+);
+
+raamatuteTabel($raamatud);
